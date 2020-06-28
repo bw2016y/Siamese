811600d15147653f036a65899bb872f9f5ec3c6d
bwy <1556019082@qq.com>
2020-06-28 21:32:23 +0800

    g4

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+    public static void main(String[] args) {
+      /*  CharStream input = CharStreams.fromString("12");
+        CalLexer  lexer = new CalLexer(input);
+
+        CommonTokenStream tokens=new CommonTokenStream(lexer);
+        CalParser parser=new CalParser(tokens);
+
+        ParseTree tree=parser.prog();
+
+        System.out.println(tree.getText());*/
+        System.out.println("aaa");
+    }
+}
